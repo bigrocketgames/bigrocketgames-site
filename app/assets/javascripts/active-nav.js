7a1d87d86ba1f87fc49c028d07c9c6aa137267f0
Nathan Robbert <admin@bigrocketgames.com>
2018-01-02 18:30:27 -0800

    feat:  Got active-nav working for about and contact nav links.  Setup about and contact pages with filler text for now.  Setup routes and controller for about and contact pages.

--- app/assets/javascripts/active-nav.js
+++ app/assets/javascripts/active-nav.js
@@ -2,8 +2,17 @@ document.addEventListener("turbolinks:load", function() {
   resetHeaderClasses();
 
   const path = window.location.pathname;
+  // const pathFound = pathSearch(path);
 
   switch(path) {
+    case '/about':
+      $('.about-link').addClass('active')
+      break;
+
+    case '/contact':
+      $('.contact-link').addClass('active')
+      break;
+      
     case '/blog':
       $('.blog-link').addClass('active')
       break;
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,10 @@ class StaticController < ApplicationController
   def home
   end
 
+  def about
+  end
+
+  def contact
+  end
+
 end
