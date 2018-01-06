972c982b120d0e9a378798e266c177435e6b9407
Nathan Robbert <admin@bigrocketgames.com>
2018-01-06 11:13:34 -0800

    feat:  Updated navbar and the active-nav.js to highlight the proper links when on those pages.

--- app/assets/javascripts/active-nav.js
+++ app/assets/javascripts/active-nav.js
@@ -2,24 +2,47 @@ document.addEventListener("turbolinks:load", function() {
   resetHeaderClasses();
 
   const path = window.location.pathname;
-  // const pathFound = pathSearch(path);
+  const pathFound = pathSearch(path);
 
-  switch(path) {
-    case '/about':
-      $('.about-link').addClass('active')
-      break;
+  if (path === "/about") {
+    $('.about-link').addClass('active');
+  } else if (path === "/contact") {
+    $('.contact-link').addClass('active');
+  } else if (pathFound === "portfolio") {
+    $('.portfolio-link').addClass('active');
+  } else if (pathFound === "blog") {
+    $('.blog-link').addClass('active');
+  } else if (pathFound === "mobile-games" || pathFound === "browser-games") {
+    $('.games-link').addClass('active');
+    findGame(path);
+  } else {
+    $('.home-link').addClass('active');
+  }
 
-    case '/contact':
-      $('.contact-link').addClass('active')
-      break;
-      
-    case '/blog':
-      $('.blog-link').addClass('active')
-      break;
+  function pathSearch(path){
 
-    default:
-      $('.home-link').addClass('active')
-      break;
+    if (path.search("/games/mobile_games") > -1) {
+      return "mobile-games";
+    } else if (path.search("/games/browser_games") > -1) {
+      return "browser-games";
+    } else if (path.search("/blog") > -1) {
+      return "blog";
+    } else if (path.search("/portfolio") > -1) {
+      return "portfolio";
+    }
+  }
+
+  function findGame(path) {
+    const game = path.split("/")[3];
+    const gameWords = game.split("-");
+    let gameInitials = ''
+
+    for (let i = 0; i < gameWords.length; i++) {
+      gameInitials += (gameWords[i][0]);
+    }
+
+    const gameClass = `${gameInitials}-game-link`
+    $(`.${gameClass}`).addClass('active');
   }
 
   function resetHeaderClasses(){
