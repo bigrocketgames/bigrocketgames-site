be703464c0b0e2a25e842ead78feaf18aa94c55e
Nathan Robbert <admin@bigrocketgames.com>
2018-01-07 22:23:58 -0800

    feat:  Updated controller and created view files.

---
+++ app/controllers/admin/browser_games_controller.rb
@@ -0,0 +1,72 @@
+class Admin::BrowserGamesController < ApplicationController
+  layout "admin"
+  before_action :authenticate_user!
+  before_action :check_user, only: [:create, :update, :destroy]
+  before_action :get_browserGame, only: [:edit, :update, :destroy]
+
+  def index
+    @browserGames = BrowserGame.all.order(:id)
+  end
+
+  def new
+  end
+
+  def create
+    browserGame = BrowserGame.new(:browserGame_params)
+
+    if browserGame.save
+      # show success message and redirect to the admin browser games list
+      flash[:success] = "Successfully created browser game!"
+      redirect_to admin_browser_games_path
+    else
+      # show error that browser game did not save and redirect back to new form.
+      flash[:alert] = "Error creating new browser game!"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @browserGame.update(:browserGame_params)
+      # show success message and redirect 
+      flash[:success] = "Successfully updated browser game!"
+      redirect_to admin_browser_games_path
+    else
+      # show error and redirect
+      flash[:alert] = "Error updating browser game!"
+      render :new
+    end
+  end
+
+  def destroy
+    if @browserGame.destroy
+      # show success message and redirect to the admin browser games list
+      flash[:success] = "Successfully deleted browser game!"
+      redirect_to admin_browser_games_path
+    else
+      # show error and redirect.
+      flash[:alert] = "Error deleting browser game!"
+      redirect_to admin_browser_games_path
+    end
+  end
+
+  private
+
+  def check_user
+    if !user_signed_in?
+      flash[:alert] = "You are not authorized for this action."
+      redirect_to root_path
+    end
+  end
+
+  def get_browserGame
+    @browserGame = BrowserGame.find_by(id: params[:id])
+  end
+
+  def browserGame_params
+    params.require(:browser_game).permit(:title, :description, :instructions)
+  end
+
+end
