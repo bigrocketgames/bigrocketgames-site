40cc4d4a13e16025ec77ab646b628e9c701a0cab
Nathan Robbert <admin@bigrocketgames.com>
2018-01-07 17:26:52 -0800

    feat: Removed the show method and added edit, update, and destroy.

--- app/controllers/admin/portfolio_controller.rb
+++ app/controllers/admin/portfolio_controller.rb
@@ -1,11 +1,9 @@
 class Admin::PortfolioController < ApplicationController
   layout "admin"
+  before_action :get_portfolio, only: [:edit, :update, :destroy]
 
   def index
     @portfolios = Portfolio.all
-  end
-
-  def show
   end
 
   def new
@@ -22,7 +20,38 @@ class Admin::PortfolioController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @portfolio.update(portfolio_params)
+      # show success message and redirect to the admin/portoflio page.
+      flash[:success] = "Successfully updated portfolio!"
+      redirect_to admin_portfolios_path
+    else
+      # show error that portfolio did not update and redirect back to the edit form.
+      flash[:alert] = "Error updating portfolio!"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @portfolio.destroy
+      # show success message and redirect to the admin portfolios page
+      flash[:success] = "Successfully deleted portfolio!"
+      redirect_to admin_portfolios_path
+    else
+      # show error that portfolio did not delete.
+      flash[:alert] = "Error deleting portfolio!"
+      redirect_to admin_portfolios_path
+    end
+  end
+
   private
+
+  def get_portfolio
+    @portfolio = Portfolio.find_by(id: params(:id))
+  end
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :description, :features, :screenshot)
