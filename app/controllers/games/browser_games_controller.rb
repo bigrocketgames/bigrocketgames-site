5fea0091518318f4a92473d182abab8afc3af761
bigrocketgames <admin@bigrocketgames.com>
2018-01-09 19:20:30 -0800

    feat: Updated index and show routes for mobile and browser game controllers.

--- app/controllers/games/browser_games_controller.rb
+++ app/controllers/games/browser_games_controller.rb
@@ -1,7 +1,11 @@
 class Games::BrowserGamesController < ApplicationController
 
+  def index
+    @browserGames = BrowserGame.all.order(:title)
+  end
+  
   def show
-
+    @browserGame = BrowserGame.find_by(id: params[:id])
   end
 
 end
--- app/controllers/games/mobile_games_controller.rb
+++ app/controllers/games/mobile_games_controller.rb
@@ -1,6 +1,11 @@
 class Games::MobileGamesController < ApplicationController
 
+  def index
+    @mobileGames = MobileGame.all.order(:title)
+  end
+
   def show
+    @mobileGame = MobileGame.find_by(id: params[:id])
   end
   
 end
