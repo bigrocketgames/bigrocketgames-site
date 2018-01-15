bbe6413ba399b97b71ff771e0619becdf3ae7bc3
bigrocketgames <admin@bigrocketgames.com>
2018-01-14 20:53:59 -0800

    feat:  Setting email settings for actionmailer.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['EMAIL_USERNAME'],
+  password: ENV['EMAIL_PASSWORD'],
+  domain: 'bigrocketgames.com',
+  address: 'mail.bigrocketgames.com',
+  port: 587,
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
