6ba50d8728ecfe9d9019547789702210f746c474
bigrocketgames <admin@bigrocketgames.com>
2018-02-15 21:56:04 -0800

    feat: Added Blog::CommentsController for comments#create.

---
+++ app/controllers/blog/comments_controller.rb
@@ -0,0 +1,20 @@
+class Blog::CommentsController < ApplicationController
+
+  def create
+    @post = Post.find_by(slug: params[:comment][:post_id])
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      ActionCable.server.broadcast "comments", render(partial: 'blog/comments/comment', object: @comment)
+    else
+      flash[:alert] = "There was an error creating the comment.  Please try again."
+      redirect_to blog_post_path(@post.slug)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :body)
+  end
+
+end
