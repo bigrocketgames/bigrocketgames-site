328db678370ae9361bda4bc2595b571edf9bc01f
Nathan Robbert <admin@bigrocketgames.com>
2018-02-19 08:39:52 -0800

    feat:  Having comment model call the job to broadcast the comment after creation.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
 
   validates :post_id, :body, presence: true
   validates :body, uniqueness: true
+
+  after_create_commit { CommentBroadcastJob.perform_later(self) }
 end
