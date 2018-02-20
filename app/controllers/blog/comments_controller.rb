f2e29fca043f55272b38a57b5878a1620195f3ce
bigrocketgames <admin@bigrocketgames.com>
2018-02-19 17:24:34 -0800

    chore:  Modifying controller to not broadcast the comment as that is now handled from model and ActiveJob.

--- app/controllers/blog/comments_controller.rb
+++ app/controllers/blog/comments_controller.rb
@@ -1,11 +1,9 @@
 class Blog::CommentsController < ApplicationController
 
   def create
-    @post = Post.find_by(slug: params[:comment][:post_id])
+    @post = Post.find(params[:comment][:post_id])
     @comment = @post.comments.build(comment_params)
-    if @comment.save
-      ActionCable.server.broadcast "comments_#{@post.slug}_channel", render(partial: 'blog/comments/comment', object: @comment)
-    else
+    if !@comment.save
       flash[:alert] = "There was an error creating the comment.  Please try again."
       redirect_to blog_post_path(@post.slug)
     end
